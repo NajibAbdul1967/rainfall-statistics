42dd739984d70022bac6a62ab864f9781b82f816
Najib Abdul <mr.neji12@hotmail.co.uk>
2018-11-19 21:21:07 +0000

    Lab work

--- src/InvalidRainfallException.java
+++
@@ -1,8 +0,0 @@
-
-public class InvalidRainfallException extends Exception {
-
-	public InvalidRainfallException(String message) {
-		// TODO Auto-generated constructor stub
-		super(message);
-	}
-}
